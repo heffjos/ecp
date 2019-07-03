c740d99a387585ca1b673fd732a30081108f20f6
Joe Heffernan <jheffheffer@gmail.com>
2019-07-03 18:05:04 -0500

    Merged base.py into workbench.py, because the file sturcutre is more manageable this way. Also changed MetricToVolumeMapping to only have ribbon-constrained mapping, because it is easier to code with nipype. I realized there needs to be a _list_outputs functions for interfaces in order for the the outputs to be written in the working directory.

--- ecp/interfaces/workbench.py
+++ ecp/interfaces/workbench.py
@@ -1,10 +1,70 @@
+import os
+
 from nipype.interfaces.base import (CommandLine, traits, CommandLineInputSpec, isdefined,
                                     File, TraitedSpec, PackageInfo)
+from nipype.utils.filemanip import fname_presuffix
 
-# from nipype.interfaces.base import (
-#     traits, TraitedSpec, BaseInterfaceInputSpec, File, Directory, isdefined,
-#     SimpleInterface
-# )
+class Info(PackageInfo):
+    """Handle workbench version information."""
+    version_cmd = 'wb_command -version'
+
+    @staticmethod
+    def parse_version(raw_info):
+        return raw_info
+
+class WBCommand(CommandLine):
+    """Base class for workbench commands."""
+    input_spec = CommandLineInputSpec
+
+    def _gen_fname(self,
+                   basename,
+                   cwd=None,
+                   suffix=None,
+                   change_ext=True,
+                   ext=None):
+        """Generate a filename based on the given parameters.
+
+        The filename will take the form: cwd/basename<suffix><ext>.
+        If change_ext is True, it will use the extentions specified in
+        <instance>intputs.output_type.
+
+        Parameters
+        ----------
+        basename : str
+            Filename to base the new filename on.
+        cwd : str
+            Path to prefix to the new filename. (default is os.getcwd())
+        suffix : str
+            Suffix to add to the `basename`.  (defaults is '' )
+        change_ext : bool
+            Flag to change the filename extension to the FSL output type.
+            (default True)
+
+        Returns
+        -------
+        fname : str
+            New filename based on given parameters.
+
+        """
+
+        if basename == '':
+            msg = 'Unable to generate filename for command %s. ' % self.cmd
+            msg += 'basename is not set!'
+            raise ValueError(msg)
+        if cwd is None:
+            cwd = os.getcwd()
+        if ext is None:
+            ext = '.nii.gz'
+        if change_ext:
+            if suffix:
+                suffix = ''.join((suffix, ext))
+            else:
+                suffix = ext
+        if suffix is None:
+            suffix = ''
+        fname = fname_presuffix(
+            basename, suffix=suffix, use_ext=False, newpath=cwd)
+        return fname
 
 class VolumeLabelImportInputSpec(CommandLineInputSpec):
     in_file = File(
@@ -42,12 +102,17 @@ class VolumeLabelImportInputSpec(CommandLineInputSpec):
 class VolumeLabelImportOutputSpec(TraitedSpec):
     out_file = File(desc='output file', exists=True)
 
-class VolumeLabelImport(CommandLine):
+class VolumeLabelImport(WBCommand):
     _cmd = 'wb_command -volume-label-import'
     input_spec = VolumeLabelImportInputSpec
     output_spec = VolumeLabelImportOutputSpec
 
-class MetricToVolumeMappingInputSpec(CommandLineInputSpec):
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        outputs['out_file'] = os.path.abspath(self.inputs.out_file)
+        return outputs
+
+class MetricToVolumeMappingRCInputSpec(CommandLineInputSpec):
     in_file = File(
         desc='the input metric file',
         argstr='%s',
@@ -74,37 +139,55 @@ class MetricToVolumeMappingInputSpec(CommandLineInputSpec):
         argstr='%s',
         position=3,
         mandatory=True)
-    nearest_vertex = traits.Float(
-        desc=('use the value from the vertex closest to the voxel\n'
-              'distance is how far from the surface to map values to voxels, in mm'),
-        argstr='-nearest-vertex %f',
-        xor=['ribbon_constrained'])
-    ribbon_constrained = traits.Tuple(
-        File(exists=True), File(exists=True),
-        desc=('use ribbon constrained mapping algorithm\n'
-              '<inner-surf> - the inner surface of the ribbon\n'
-              '<outer-surf> - the outer surface of the ribbon'),
-        argstr='-ribbon-constrained %s %s')
+    inner_surf = File(
+        desc='inner surface of the ribbon',
+        argstr='-ribbon-constrained %s',
+        position=4,
+        mandatory=True,
+        exists=True,
+        copyfile=False)
+    outer_surf = File(
+        desc='outer surface of the ribbon',
+        argstr='%s',
+        position=5,
+        mandatory=True,
+        exists=True,
+        copyfile=False)
     voxel_subdiv = traits.Int(
         desc=('voxel division while estimating voxel weights\n'
               '<subdiv-num> number of subdivisions, default 3'),
-        argstr='-voxel-subdiv %d',
-        requires=['ribbon_constrained'])
+        argstr='-voxel-subdiv %d')
     greedy = traits.Bool(
         desc=('instead of antialiasing partial-volumed voxels, put full\n'
               'metric values (legacy behavior)'),
-        argstr='-greedy',
-        requires=['ribbon_constrained'])
+        argstr='-greedy')
     thick_columns = traits.Bool(
         desc = 'use overlapping columns (legacy method)',
-        argstr='-thick-columns',
-        requires=['ribbon_constrained'])
+        argstr='-thick-columns')
 
-class MetricToVolumeMappingOutputSpec(TraitedSpec):
+class MetricToVolumeMappingRCOutputSpec(TraitedSpec):
     out_file = File(desc='output file', exists=True)
 
-class MetricToVolumeMapping(CommandLine):
+class MetricToVolumeMappingRC(CommandLine):
     _cmd = 'wb_command -metric-to-volume-mapping'
-    input_spec = MetricToVolumeMappingInputSpec
-    output_spec = MetricToVolumeMappingOutputSpec
-  
+    input_spec = MetricToVolumeMappingRCInputSpec
+    output_spec = MetricToVolumeMappingRCOutputSpec
+
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        outputs['out_file'] = os.path.abspath(self.inputs.out_file)
+        return outputs
+
+def check_wb():
+    ver = Info.version()
+    if ver:
+        return 0
+    else:
+        return 1
+
+def no_wb():
+    """Checks if wb_command is available"""
+    if Info.version() is None:
+        return True
+    return False
+
