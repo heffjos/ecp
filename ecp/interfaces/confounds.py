de191cf69748a84d980fcb5f383677011f5f62fb
Joe Heffernan <jheffheffer@gmail.com>
2019-07-07 19:23:38 -0500

    Starting adding an interface to convert the regressors tsv to 1D files. I stopped because I think this would be better to have it as inlince code in a workflow.

--- ecp/interfaces/confounds.py
+++ ecp/interfaces/confounds.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pandas as pd
 
 from nipype.interfaces.base import (
     traits, TraitedSpec, BaseInterfaceInputSpec, File, Directory, isdefined,
@@ -7,13 +8,15 @@ from nipype.interfaces.base import (
 )
 
 class GetHcpMovementInputSpec(BaseInterfaceInputSpec):
-    hcp_movement = File(desc='HCP movement regressors', exists=True)
+    hcp_movement = File(desc='HCP movement regressors', 
+                        exists=True,
+                        mandatory=True)
 
 class GetHcpMovementOutputSpec(TraitedSpec):
     movement = File(desc='movement regressors', exists=True)
 
 class GetHcpMovement(SimpleInterface):
-    """Converts HCP movement regressor file to ECP regressor files."""
+    """Grabs the 6 motion parmaters from the HCP motion file"""
     input_spec = GetHcpMovementInputSpec
     output_spec = GetHcpMovementOutputSpec
 
@@ -31,3 +34,47 @@ class GetHcpMovement(SimpleInterface):
 
         return runtime
 
+class RegressorsTsvTo1DInputSpec(BaseInterfaceInputSpec):
+    regressors = File(desc='regressors.tsv file', 
+                      exists=True, 
+                      mandatory=True)
+
+class RegressorsTsvTo1DOutputSpec(TraitedSpec):
+    movement = File(desc='movement parameters', exists=True)
+    movement_sq = File(desc='movement paratmers squared', exists=True)
+    movement_deriv = File(desc='movement derivatives', exists=True)
+    movement_deriv_sq = File(desc='movement derivatives squared', exists=True)
+
+    csf = File(desc='mean csf time course', exists=True)
+    wm = File(desc='mean wm time course', exists=True)
+    gs = File(desc='global signal time course', exists=True)
+
+    csf_deriv = File(desc='mean csf derivative', exists=True)
+    wm_deriv = File(desc='mean wm derivative', exists=True)
+    gs_deriv = File(desc='mean global signal derivative', exists=True)
+
+    csf_sq = File(desc='mean csf squared', exists=True)
+    wm_sq = File(desc='mean wm squared', exists=True)
+    gs_sq = File(desc='mean gs squared', exists=True)
+
+    csf_deriv_sq = File(desc='mean csf derivative squared', exists=True)
+    wm_deriv_sq = File(desc='mean wm derivative squared', exists=True)
+    gs_sq = File(desc='mean gs derivative squared', exists=True)
+
+    tcc = File(desc='temporal comp cor', exists=True)
+    acc = File(desc='anatomical comp cor', exists=True)
+    
+    fd = File(desc='frame displacement', exists=True)
+    std_dvars = File(desc='std dvars', exists=True)
+    dvars = File(desc='dvars', exists=True)
+
+class RegressorsTsvTo1D(SimpleInterface):
+    """Converts the regressors.tsv file to 1D files for 3dTproject"""
+    input_spec = RegressorsTsvTo1DInputSpec
+    output_spec = RegressorsTsvTo1DOuptuSpec
+
+    def _run_interface(self, runtime):
+        out_dir = runtime.cwd
+        data = pd.read_csv(self.inputs.regressors, sep='\t')
+
+        return runtime
