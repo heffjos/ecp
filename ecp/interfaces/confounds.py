91063b843c5ed473de1cb946c5f5e69678ae8e87
Joe Heffernan <jheffheffer@gmail.com>
2019-07-10 12:44:41 -0500

    Added interface RegressorsTsvTo1DOutputSpec. This may never be used, because I have decided to convert the regressors csv to 1D files in the cli script.

--- ecp/interfaces/confounds.py
+++ ecp/interfaces/confounds.py
@@ -1,4 +1,5 @@
 import os
+import re
 import numpy as np
 import pandas as pd
 
@@ -59,7 +60,7 @@ class RegressorsTsvTo1DOutputSpec(TraitedSpec):
 
     csf_deriv_sq = File(desc='mean csf derivative squared', exists=True)
     wm_deriv_sq = File(desc='mean wm derivative squared', exists=True)
-    gs_sq = File(desc='mean gs derivative squared', exists=True)
+    gs_deriv_sq = File(desc='mean gs derivative squared', exists=True)
 
     tcc = File(desc='temporal comp cor', exists=True)
     acc = File(desc='anatomical comp cor', exists=True)
@@ -71,10 +72,80 @@ class RegressorsTsvTo1DOutputSpec(TraitedSpec):
 class RegressorsTsvTo1D(SimpleInterface):
     """Converts the regressors.tsv file to 1D files for 3dTproject"""
     input_spec = RegressorsTsvTo1DInputSpec
-    output_spec = RegressorsTsvTo1DOuptuSpec
+    output_spec = RegressorsTsvTo1DOutputSpec
 
     def _run_interface(self, runtime):
         out_dir = runtime.cwd
         data = pd.read_csv(self.inputs.regressors, sep='\t')
+        file_table = {
+            'csf': 'csf',
+            'wm': 'white_matter',
+            'gs': 'global_signal',
+            'csf_deriv': 'csf_derivative1',
+            'wm_deriv': 'white_matter_derivative1',
+            'gs_deriv': 'global_signal_derivative1',
+            'csf_sq': 'csf_power2',
+            'wm_sq': 'white_matter_power2',
+            'gs_sq': 'global_singal_power2',
+            'csf_deriv_sq': 'csf_derivative1_power2',
+            'wm_deriv_sq': 'white_matter_derivative1_power2',
+            'gs_deriv_sq': 'global_signal_derivative1_power2',
+            'fd': 'framewise_displacement',
+            'std_dvars': 'std_dvars',
+            'dvars': 'dvars'
+        }
+
+        pd.to_csv(
+            os.path.join(out_dir, 'movement.1D'),
+            data[[x for x in data.columns if re.match('^(rot|trans)_[xyz]$', x)]],
+            header=False, 
+            index=False,
+            sep=' ')
+        self._results['movement'] = os.path.join(out_dir, 'movement.1D')
+
+        pd.to_csv(
+            os.path.join(out_dir, 'movement_sq.1D'),
+            data[[x for x in data.columns if re.match('^(rot|trans)_[xyz]_power2$', x)]],
+            header=False, 
+            index=False,
+            sep=' ')
+        self._results['movement_sq'] = os.path.join(out_dir, 'movement_sq.1D')
+
+        pd.to_csv(
+            os.path.join(out_dir, 'movement_deriv.1D'),
+            data[[x for x in data.columns if re.match('^(rot|trans)_[xyz]_derivative1$', x)]],
+            header=False, 
+            index=False,
+            sep=' ')
+        self._results['movement_deriv'] = os.path.join(out_dir, 'movement_deriv.1D')
+
+        pd.to_csv(
+            os.path.join(out_dir, 'movement_deriv_sq.1D'),
+            data[[x for x in data.columns if re.match('^(rot|trans)_[xyz]_derivative1_power2$', x)]],
+            header=False, 
+            index=False,
+            sep=' ')
+        self._results['movement_deriv_sq'] = os.path.join(out_dir, 'movement_deriv_sq.1D')
+
+        for file_name, column in file_table.items():
+            out_file = os.path.join(out_dir, file_name + '.1D')
+            pd.to_csv(out_file, data[column], header=False, index=False, sep=' ')
+            self._results[file_name] = out_file
+
+        pd.to_csv(
+            os.path.join(out_dir, 'tcc.1D'),
+            data[[x for x in data.columns if re.match('^t_comp_cor_\d+$', x)]],
+            header=False,
+            index=False,
+            sep=' ')
+        self._results['tcc'] = os.path.join(out_dir, 'tcc.1D')
+
+        pd.to_csv(
+            os.path.join(out_dir, 'acc.1D'),
+            data[[x for x in data.columns if re.match('^a_comp_cor_\d+$', x)]],
+            header=False,
+            index=False,
+            sep=' ')
+        self._results['acc'] = os.path.join(out_dir, 'acc.1D')
 
         return runtime
